368b305474565526546270692d462dcf17c736fa
Narezka <nazerke99.kg@gmail.com>
2023-04-18 21:58:57 +0800

    changes

--- src/axios.js
+++ src/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 
 export const makeRequest = axios.create({
-  baseURL: "https://social1-app.herokuapp.com/api/",
+  baseURL: "https://social-app-heroku.herokuapp.com/api/",
   withCredentials: true,
 });
