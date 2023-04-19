c402520ee5ce508b787e7dc018574b81b4cfea7b
Narezka <nazerke99.kg@gmail.com>
2023-04-19 19:41:53 +0800

    old data back

--- src/axios.js
+++ src/axios.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
-// export const makeRequest = axios.create({
-//   baseURL: "https://social-app-heroku.herokuapp.com/api/",
-//   withCredentials: true,
-// });
+export const makeRequest = axios.create({
+  baseURL: "https://social-app-heroku.herokuapp.com/api/",
+  withCredentials: true,
+});
 
-export const makeRequest = fetch(
-  "https://social-app-heroku.herokuapp.com/api/",
-  {
-    method: "GET",
-    credentials: "include",
-  }
-).then((res) => {});
+// export const makeRequest = fetch(
+//   "https://social-app-heroku.herokuapp.com/api/",
+//   {
+//     method: "GET",
+//     credentials: "include",
+//   }
+// ).then((res) => {});
