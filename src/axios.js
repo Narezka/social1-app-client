706a417bc6a05e0ecf77246e45931566edbdb469
Narezka <nazerke99.kg@gmail.com>
2023-04-19 19:37:43 +0800

    fetch in axios

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,14 @@
 import axios from "axios";
 
+// export const makeRequest = axios.create({
+//   baseURL: "https://social-app-heroku.herokuapp.com/api/",
+//   withCredentials: true,
+// });
 
-export const makeRequest = axios.create({
-  baseURL: "https://social-app-heroku.herokuapp.com/api/",
-  withCredentials: true,
-});
+export const makeRequest = fetch(
+  `${social - app - heroku.herokuapp.com}/api/`,
+  {
+    method: "GET",
+    credentials: "include",
+  }
+).then((res) => {});
